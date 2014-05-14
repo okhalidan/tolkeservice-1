f43793a45e2ddee9a767321e4b91a4c8c51efa1c
okhalidan <khalidan@mac.com>
2014-05-14 12:32:47 +0200

    hej morten - dette er bare en test -m

---
+++ removeComma.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class removeComma {
+
+	public static void main(String[] args) {
+		// Spørger brugeren om at indtaste en værdi.
+		System.out.print("Please enter an integer between 1,000 and 999,999:");
+		// Registrer hvad der bliver tastet på keyboard.
+		Scanner in = new Scanner(System.in);
+		// læser værdien og gemmer den som en streng
+		String inputNumber = in.nextLine();
+		// giver længden af værdien som er indtastet.
+		int lengthNumber = inputNumber.length();
+		// 
+		String outputNumber = inputNumber.substring(0, lengthNumber-4) +
+				inputNumber.substring(lengthNumber-3);
+		// udskriver med ny linje den nye værdi uden kommaer.
+		System.out.println(outputNumber);
+	}
+}
